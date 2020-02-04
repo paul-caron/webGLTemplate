285a913c6292df090450b5bb93ed436b37626437
Paul Caron <45680511+paul-caron@users.noreply.github.com>
2020-02-04 00:02:25 -0600

    update points.js

--- points.js
+++ points.js
@@ -1,215 +1,57 @@
 // Created by Paul Caron. No copy authorized
 
-class Matrix{
-    constructor(rows, columns){
-        this.rows=rows;
-        this.columns=columns;
-        this.values=new Array(rows*columns);
-    }
-    setValues(values){
-        for(let a=0;a<values.length;a++){
-            this.values[a]=values[a];
-        }
-    }    
-    printValues(mat){
-        let rowstr = "";
-        let index=0;
-        for(let a=0;a<this.rows;a++){
-            rowstr="";
-            for(let b=0;b<this.columns;b++){
-                rowstr+=this.values[index++].toFixed(7)+" ";               
-            }
-            console.log(rowstr);
-        }
-    }
-    resetToNull(){
-        for(let a=0;a<this.values.length;a++){
-            this.values[a]=0;
-        }    
-    }
-    makeIdentity(){        
-        if(this.rows===this.columns){
-            this.resetToNull();
-            for(let a=0;a<this.rows;a++){
-                this.values[a*this.columns+a]=1;
-            }
-        }
-    }
-    ///statics
-    static add(mat1, mat2){
-        if(mat1.rows===mat2.rows&&mat1.columns===mat2.columns){
-            let result = new Matrix(mat1.rows, mat1.columns);
-            for(let a=0;a<mat1.values.length;a++){
-                result.values[a]=mat1.values[a]+mat2.values[a];                
-            }          
-            return result;
-        }
-    }
-    static kmult(mat,k){
-        let result=new Matrix(mat.rows,mat.columns 
-        );
-        for(let a=0;a<mat.values.length;a++){
-            result.values[a]=(mat.values[a]*k);                        
-        }
-        return result ;
-    }
-    static mult(mat1,mat2){
-        if(mat1.columns===mat2.rows){
-            let result= new Matrix(mat1.rows, mat2.columns);
-            let index = 0;
-            let one=0;
-            let two=0;
-            for(let h=0;h<mat1.rows;h++){
-            for(let i=0;i<mat2.columns;i++){
-                result.values[index]=0;
-                for(let j=0;j<mat1.columns;j++){
-                    one=mat1.values[j+h*mat1.columns];
-                    two=mat2.values[j*mat2.columns+i];
-                    ;
-                    result.values[index] += one * two;               
-                }
-                index++;
-            }
-            }            
-            return result;           
-        }
-    }
-    static transpose(mat){
-        let result=new Matrix(mat.columns,mat.rows);
-        for(let index=0;index<mat.values.length;index++){
-            result.values[(index%result.rows)*result.columns+parseInt(index/result.rows)]=mat.values[index];        
-        }
-        return result;
-    }
-    static inverse(mat){
-        if(mat.rows===mat.columns){
-            
-            if(mat.rows==2){
-                let adjugate=new Matrix(mat.rows, mat.columns);
-                let a=Matrix.getValueByRC(mat,0,0);
-                let b=Matrix.getValueByRC(mat,0,1);
-                let c=Matrix.getValueByRC(mat,1,0);
-                let d=Matrix.getValueByRC(mat,1,1);
-                let det=a*d-b*c;
-                adjugate.setValues([d,-b, -c, a]);
-                let inverse=Matrix.kmult(adjugate,1/det);
-                return inverse;
-            }else{
-                //get matrix of minors
-                let min=Matrix.minors(mat);
-                //add cofactors
-                Matrix.addCofactors(min);
-                //transpose
-                let adjugate=Matrix.transpose(min);
-                //multiply by determinant
-                let det = Matrix.determinant(mat);                              
-                let inverse=Matrix.kmult(adjugate,1/det);
-                return inverse;
-            }            
-        }
-    }
-    static minors(mat){        
-        let result=[];               
-        if(mat.rows==2&&mat.columns==2){
-            let a=Matrix.getValueByRC(mat,0,0);
-                let b=Matrix.getValueByRC(mat,0,1);
-                let c=Matrix.getValueByRC(mat,1,0);
-                let d=Matrix.getValueByRC(mat,1,1);
-                let det=a*d-b*c;
-            return det;
-        }else{
-        if(mat.rows===mat.columns){            
-            for(let a=0;a<mat.rows;a++){
-                for(let b=0;b<mat.columns;b++){
-                    let det=Matrix.determinant(Matrix.removeRC(mat,a,b));
-                    result.push(det);                
-                }
-            }
-            let min= new Matrix(mat.rows,mat.columns);
-            min.setValues(result);
-            return min;                        
-        }
-        }
-    }
-    static getValueByRC(mat, row, col){
-        return mat.values[row*mat.columns + col];
-    }
-    static setValueByRC(mat,row,col,val){
-        mat.values[row*mat.columns + col]=val;
-    }
-    
-    static determinant(mat){
-        if(mat.rows==2){
-            let a=Matrix.getValueByRC(mat,0,0);
-            let b=Matrix.getValueByRC(mat,0,1);
-            let c=Matrix.getValueByRC(mat,1,0);
-            let d=Matrix.getValueByRC(mat,1,1);
-            let det=a*d-b*c;
-            return det;
-        }else{
-            let result=[];
-            let mat2= new Matrix(mat.rows-1,mat.columns-1);
-            for(let a=0;a<mat.rows;a++){
-                for(let b=0;b<mat.columns;b++){
-                    result.push(Matrix.removeRC(mat,a,b));
-                }
-            }                     
-            let result2=[];
-            for(let r of result){
-                let matz=new Matrix(mat.rows-1,mat.columns-1);
-                matz.setValues(r.values);
-                result2.push(Matrix.determinant(matz));               
-            }
-            //cofactors
-            let det=0;
-            let cof=1;
-            for(let a=0;a<mat.columns;a++){
-                det+=result2[a]*cof*mat.values[a];
-                cof*=-1;
-            }            
-            return det; 
-        }
-    }
-    static removeRC(mat,row,col){
-        let result = new Matrix(mat.rows-1,mat.columns-1);
-        let index=0;
-        for(let a=0;a<mat.rows;a++){
-            for(let b=0;b<mat.columns;b++){
-                if(a===row||b==col){
-                    
-                }else{                    
-                    result.values[index]=Matrix.getValueByRC(mat,a,b);
-                    index++;
-                }
-            }
-        }
-        return result;
-    }
-    static addCofactors(mat){
-        for(let a=0;a<mat.rows;a++){
-            for(let b=0;b<mat.columns;b++){                
-                if((a%2===0&&b%2===1)||(a%2===1&&b%2===0)){
-                    let value= Matrix.getValueByRC(mat,a,b);
-                    Matrix.setValueByRC(mat,a,b,value*-1);
-                }
-            }
-        }
-    }
-}//End of the Matrix Class
+// Created by Paul Caron, february 2020. No copy authorized.
+
+////////////////////////////////////////
+//WebGL stuff...Shader codes and whatnot
+///////////////////////////////////////
+
+var vertexShaderCode = [
+"precision mediump float;",
+"",
+"attribute vec4 vertPosition;",//attributes are like parameters input unique to each vertex
+"attribute vec3 vertColor;",
+"varying vec3 fragColor;",//varying are like outputs that goes to frag shader
+"",
+//uniform are like parameter input that are the same for all vertices
+"uniform mat4 mProj;",
+"uniform mat4 mWorldX;",
+"uniform mat4 mWorldY;",
+"uniform mat4 mWorldZ;",
+"uniform mat4 mTranslation;",
+
+"void main()",
+"{",
+"    gl_Position =mProj* mTranslation*mWorldY* mWorldX *mWorldZ * vertPosition;",
+"    gl_PointSize = sqrt(2.0*pow(gl_Position.x,2.0));",
+"    fragColor = vertColor-0.001*(gl_Position.z*gl_Position.z) ;",
+"}"
+].join("\n");
+
+var fragmentShaderCode = [
+"precision mediump float;",
+"",
+"varying vec3 fragColor;",
+"void main()",
+"{",
+"    gl_FragColor = vec4(fragColor, 1.0);",
+"}"
+].join("\n");
+
+var canvas;
+var gl;
 
 
-
-
-
+function init(){
 ////////////////////////
 //rotation matrices
 ////////////////////////
 //X
-var rotationX=new Matrix(4,4);
+let rotationX=new Matrix(4,4);
 rotationX.setAngle = function(angle){
     this.angle = angle;
     this.setValues([
-    1,0,0,    0,
+    1,0,0, 0,
     0,Math.cos(this.angle),-Math.sin(this.angle),0,
     0,Math.sin(this.angle),Math.cos(this.angle),0,
     0,0,0,1
@@ -217,11 +59,11 @@ rotationX.setAngle = function(angle){
 }
 rotationX.makeIdentity();
 //Y
-var rotationY=new Matrix(4,4);
+let rotationY=new Matrix(4,4);
 rotationY.setAngle = function(angle){
     this.angle = angle;
     this.setValues([
-    Math.cos(this.angle),0,Math.sin(this.angle),    0,
+    Math.cos(this.angle),0,Math.sin(this.angle), 0,
     0,1,0,0,
     -Math.sin(this.angle),0,Math.cos(this.angle),0,
     0,0,0,1
@@ -230,11 +72,11 @@ rotationY.setAngle = function(angle){
 rotationY.makeIdentity();
 rotationY.setAngle(Math.PI);
 //Z
-var rotationZ=new Matrix(4,4);
+let rotationZ=new Matrix(4,4);
 rotationZ.setAngle = function(angle){
     this.angle = angle;
     this.setValues([
-    Math.cos(this.angle),Math.sin(this.angle),0,    0,
+    Math.cos(this.angle),Math.sin(this.angle),0, 0,
     -Math.sin(this.angle),Math.cos(this.angle),0, 0,
     0,0,1,0,
     0,0,0,1
@@ -243,7 +85,7 @@ rotationZ.setAngle = function(angle){
 rotationZ.makeIdentity();
 
 /////
-var translation = new Matrix(4,4);
+let translation = new Matrix(4,4);
 translation.makeIdentity();
 translation.setT = function(x,y,z){
     translation.values[3] += x;
@@ -251,15 +93,14 @@ translation.setT = function(x,y,z){
     translation.values[11] += z;
 }
 translation.setT(0,0,-15);
-translation =Matrix.transpose(Matrix.inverse(Matrix.mult(translation,rotationY)));
+translation =Matrix.transpose(Matrix.inverse(Matrix.mult(translation,rotationY)));//flip camera to look from back of the scene
 
 //////////////
 //projection
 //////////////
-let projection = new Matrix(4,4);
 let p;
 function setProjection(aspectRatio,fov,nearZ,farZ){
-    var  pmat = new Matrix(4,4);
+    let pmat = new Matrix(4,4);
     
     p= [
     (1/(aspectRatio*Math.tan(fov/2))),0,0,0,
@@ -288,58 +129,12 @@ function makeShapes(){
     }
 }
 makeShapes();
-////////////////////////////////////////
-//WebGL stuff...Shader codes and whatnot
-///////////////////////////////////////
-
-var vertexShaderCode = [
-"precision mediump float;",
-"",
-"attribute vec4 vertPosition;",//attributes are like parameters input unique to each vertex
-"attribute vec3 vertColor;",
-"varying vec3 fragColor;",//varying are like outputs that goes to frag shader
-"",
-"uniform mat4 mWorld;",//uniform are like parameter input that are the same for all vertices
-"uniform mat4 mView;",
-"uniform mat4 mProj;",
-"uniform mat4 mWorldX;",
-"uniform mat4 mWorldY;",
-"uniform mat4 mWorldZ;",
-"uniform mat4 mProjection;",
-"uniform mat4 mTranslation;",
-"uniform mat4 mScale;",
-"void main()",
-"{",
 
 
-"    gl_Position =mProj* mTranslation*mWorldY* mWorldX *mWorldZ * vertPosition;",
-"    gl_PointSize = sqrt(2.0*pow(gl_Position.x,2.0));",
-"    fragColor = vertColor-0.001*(gl_Position.z*gl_Position.z) ;",
-"}"
-].join("\n");
-
-var fragmentShaderCode = [
-"precision mediump float;",
-"",
-"varying vec3 fragColor;",
-"void main()",
-"{",
-"    gl_FragColor = vec4(fragColor, 1.0);",
-"}"
-].join("\n");
-
-var canvas;
-var gl;
-
-
-function init(){
-    
     canvas = document.getElementById("canvas");
     canvas.width=innerWidth-10;
     canvas.height=innerHeight-10;
-    gl = canvas.getContext("webgl", {preserveDrawingBuffer: true});
-    
-    
+    gl = canvas.getContext("webgl", {preserveDrawingBuffer: false});
     if(!gl){
         gl = canvas.getContext("experimental-webgl");
     }
@@ -350,8 +145,8 @@ function init(){
     //SHADERS
     ////////////////////
     //Create Shaders
-    var vertexShader = gl.createShader(gl.VERTEX_SHADER);
-    var fragmentShader = gl.createShader(gl.FRAGMENT_SHADER);
+    const vertexShader = gl.createShader(gl.VERTEX_SHADER);
+    const fragmentShader = gl.createShader(gl.FRAGMENT_SHADER);
     //add shader source code
     gl.shaderSource(vertexShader, vertexShaderCode );
     gl.shaderSource(fragmentShader, fragmentShaderCode);
@@ -381,11 +176,11 @@ function init(){
     ////////////////////
     //Buffers
     ////////////////////            
-    var vertexBufferObject = gl.createBuffer();
+    const vertexBufferObject = gl.createBuffer();
     gl.bindBuffer(gl.ARRAY_BUFFER, vertexBufferObject);
     gl.bufferData(gl.ARRAY_BUFFER, new Float32Array(vertices), gl.STATIC_DRAW);        
-    var positionAttribLocation = gl.getAttribLocation(program, "vertPosition");
-    var colorAttribLocation = gl.getAttribLocation(program, "vertColor");
+    const positionAttribLocation = gl.getAttribLocation(program, "vertPosition");
+    const colorAttribLocation = gl.getAttribLocation(program, "vertColor");
     gl.vertexAttribPointer(
         positionAttribLocation,
         4,//number of elements
@@ -405,22 +200,16 @@ function init(){
     gl.enableVertexAttribArray(positionAttribLocation);
     gl.enableVertexAttribArray(colorAttribLocation);
     gl.useProgram(program);
-    
-    var mViewUniformLocation = gl.getUniformLocation(program, "mView");
-    var mProjUniformLocation = gl.getUniformLocation(program, "mProj");
-    var mProjectionUniformLocation = gl.getUniformLocation(program, "mProjection");
-    var mWorldXUniformLocation = gl.getUniformLocation(program, "mWorldX");
-    var mWorldYUniformLocation = gl.getUniformLocation(program, "mWorldY");
-    var mWorldZUniformLocation = gl.getUniformLocation(program, "mWorldZ");
-    var mTranslationUniformLocation = gl.getUniformLocation(program, "mTranslation");
 
-    var viewMatrix = new Float32Array(16);
+    const mProjUniformLocation = gl.getUniformLocation(program, "mProj");
+    const mWorldXUniformLocation = gl.getUniformLocation(program, "mWorldX");
+    const mWorldYUniformLocation = gl.getUniformLocation(program, "mWorldY");
+    const mWorldZUniformLocation = gl.getUniformLocation(program, "mWorldZ");
+    const mTranslationUniformLocation = gl.getUniformLocation(program, "mTranslation");
+
   setProjection(canvas.width/canvas.height,Math.PI/1.5,1,1000.0);
 
-    gl.uniformMatrix4fv(mViewUniformLocation, gl.FALSE, viewMatrix );
     gl.uniformMatrix4fv(mProjUniformLocation, gl.FALSE, p);
-    gl.uniformMatrix4fv(mProjectionUniformLocation, gl.FALSE, projection.values );
-    ///
     gl.uniformMatrix4fv(mWorldXUniformLocation, gl.FALSE, rotationX.values );
     gl.uniformMatrix4fv(mWorldYUniformLocation, gl.FALSE, rotationY.values );
     gl.uniformMatrix4fv(mWorldZUniformLocation, gl.FALSE, rotationZ.values );
@@ -439,7 +228,7 @@ function init(){
     function loop(time){  
         k=Math.cos(time/10000)*10.0;
         makeShapes();     
-        rotationZ.setAngle(-time/5000);
+        //rotationZ.setAngle(-time/5000);
         rotationY.setAngle(time/10000);
         rotationX.setAngle(time/15000);
         gl.bufferData(gl.ARRAY_BUFFER, new Float32Array(vertices), gl.STATIC_DRAW);           
@@ -449,13 +238,14 @@ function init(){
         gl.clear(gl.COLOR_BUFFER_BIT|gl.DEPTH_BUFFER_BIT);
    
         gl.uniformMatrix4fv(mWorldXUniformLocation, gl.FALSE, rotationX.values );
-        gl.uniformMatrix4fv(mWorldYUniformLocation, gl.FALSE, rotationY.values );
+    gl.uniformMatrix4fv(mWorldYUniformLocation, gl.FALSE, rotationY.values );
         gl.uniformMatrix4fv(mWorldZUniformLocation, gl.FALSE, rotationZ.values );
     
         //draw
         gl.drawArrays(gl.POINTS, 0, vertices.length/7);     
         requestAnimationFrame(loop);
     }
-   requestAnimationFrame(loop);
+    requestAnimationFrame(loop);
 }
 
+window.onload = init;
